bb20d6eb408afa3d3ae7a52f389d4311ccdaad99
jam-odoo <jam@odoo.com>
2015-11-01 16:41:53 -0800

    Added Done Restore logic
    updated restore logic, restore will also load done state, store that in
    localStore

--- todo.js
+++ todo.js
@@ -4,15 +4,25 @@ function getFromLocalStorage(){
 }
 function addToLocalStorage(name){
     var storedTodos = getFromLocalStorage();
-    storedTodos.push(name)
+    storedTodos.push({"name": name, "done": false})
     localStorage.todoList = JSON.stringify(storedTodos);
 }
 function popFromLocalStorage(name){
     var storedTodos = getFromLocalStorage();
     //find index make sure right index and splice it.
-    var idx = storedTodos.indexOf(name);
-    if (idx > -1) {
-        storedTodos.splice(idx, 1);
+    for (item in  storedTodos){
+        if (storedTodos[item].name === name){
+            storedTodos.splice(item, 1);
+        }
+    }
+    localStorage.todoList = JSON.stringify(storedTodos);
+}
+function updateLocalStorage(name){
+    var storedTodos = getFromLocalStorage();
+    for (item in  storedTodos){
+        if (storedTodos[item].name === name){
+            storedTodos[item].done = true;
+        }
     }
     localStorage.todoList = JSON.stringify(storedTodos);
 }
@@ -33,10 +43,12 @@ function todoApplication(){
     function doneItem(){
         if (this.parentElement.firstChild){
             this.parentElement.firstChild.classList.add("completed");
+            this.parentElement.setAttribute("data-done", "true");
+            var node = this.parentNode.firstElementChild.textContent
+            updateLocalStorage(node)
         }
         this.style.visibility = "hidden";
     }
-
     function prepareActionNode(class_name, text, action){
         var node = document.createElement('a');
         node.setAttribute("href", "#");
@@ -45,40 +57,41 @@ function todoApplication(){
         node.addEventListener("click", action);
         return node
     }
-
-    function addItem(name, toLocalStore){
+    function addItem(name, toLocalStore, done){
         var newNode = document.createElement('li');
         var nodeSpan = document.createElement('span');
+        var doneNode = prepareActionNode("done-item", "done", doneItem);
         nodeSpan.innerHTML = name
-        nodeSpan.setAttribute("class", "li-text-label");
+        nodeSpan.setAttribute("data-done", "false");
         nodeSpan.setAttribute("id", "li-text-label");
+        if(done){
+            nodeSpan.setAttribute("class", "completed");
+            doneNode.style.visibility = 'hidden';
+        }
         newNode.setAttribute("id", "todolist-item");
         newNode.appendChild(nodeSpan);
         newNode.appendChild(prepareActionNode("unlink-item", "remove", unlinkItem));
-        newNode.appendChild(prepareActionNode("done-item", "done", doneItem));
+        newNode.appendChild(doneNode);
         todoList.appendChild(newNode);
-        //Extra condition to not add duplicate and crab again 
-        //restore state addItem call.
+        //Extra condition to not add duplicate and crab again restore state addItem call.
         if (toLocalStore){
             addToLocalStorage(itemValue.value)
         }
     }
-
     function appendToList(event) {
         if (itemValue.value !== ''){
-            addItem(itemValue.value, true);
+            addItem(itemValue.value, true, false);
         }
         itemValue.value = "";
         itemValue.focus();
         // event.preventDefault();
     }
-
     // Restore state function on load
     function restoreState(){
         var items = getFromLocalStorage();
         for (item in items){
             if (items[item] !== '' || items[item] !== undefined){
-                addItem(items[item], false)
+                addItem(items[item].name, false, items[item].done)
             }
         }
     }
